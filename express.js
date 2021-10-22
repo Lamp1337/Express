8861a1ac209fe00bf8e713ea2159088a232fc2f6
Lamp <90402655+Lamp1337@users.noreply.github.com>
2021-10-22 07:18:56 -0700

    Update express.js

--- express.js
+++ express.js
@@ -1,16 +1,1 @@
-// IF ERROR DM Lamp#1442
-const express = require("express");
-const prompt = require("prompt-sync")();
-const app = express();
-const ip = prompt("Server IP : ")
-const port = prompt("Server Port (17091) : ")
-var packet = `server|${ip}\nport|${port}\ntype|1\n#maint|Server is currently under maintanance We will back ASAP\n\nbeta_server|${ip}\nbeta_port|${port}\n\nbeta_type|1\nmeta|localhost\nRTENDMARKERBS1001`;
-
-app.post('/growtopia/server_data.php', (res) => {
-    res.writeHead(200, {"Content-Type": "text/html"} );
-    res.send(packet);
-    res.end();
-    res.destroy();
-});
-
-app.listen(80)
+//LAGI FIX
